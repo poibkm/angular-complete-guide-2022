61d008b40871d4dcf04595097086b776c72edb6d
Manuel Brestak <manuel.brestak@porscheinformatik.com>
2022-08-12 14:12:21 +0200

    22. Working with Component Templates

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-servers',
-  templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  selector: "app-servers",
+  template: "<app-server></app-server><app-server></app-server>",
+  styleUrls: ["./servers.component.css"],
 })
 export class ServersComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
