5eb5f31db30a58a42eff2f2577ce3d292f122dd3
Manuel Brestak <manuel.brestak@porscheinformatik.com>
2022-08-19 14:40:06 +0200

    114. A Different Way Of Injecting Services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,11 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { AccountComponent } from "./account/account.component";
 import { NewAccountComponent } from "./new-account/new-account.component";
-import { AccountsService } from "./shared/accounts.service";
-import { LoggingService } from "./shared/logging.service";
 
 @NgModule({
   declarations: [AppComponent, AccountComponent, NewAccountComponent],
   imports: [BrowserModule, FormsModule],
-  providers: [AccountsService, LoggingService],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
