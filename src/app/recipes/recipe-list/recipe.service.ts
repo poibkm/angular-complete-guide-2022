cc587ad272e2e836d4c2690618e68c864e8253b7
Manuel Brestak <manuel.brestak@porscheinformatik.com>
2022-08-19 16:14:23 +0200

    122. Adding Ingredients to Recipes

--- src/app/recipes/recipe-list/recipe.service.ts
+++ src/app/recipes/recipe-list/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "src/app/shared/ingredient.model";
 import { Recipe } from "../recipe.model";
 
 @Injectable({
@@ -11,12 +12,14 @@ export class RecipeService {
     new Recipe(
       "A Test Recipe",
       "This is simply a test recipe",
-      "https://images.unsplash.com/photo-1490645935967-10de6ba17061?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1453&q=80"
+      "https://images.unsplash.com/photo-1490645935967-10de6ba17061?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1453&q=80",
+      [new Ingredient("Meat", 1), new Ingredient("French Fries", 20)]
     ),
     new Recipe(
       "Another Test Recipe",
       "This is simply a test recipe",
-      "https://images.unsplash.com/photo-1490645935967-10de6ba17061?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1453&q=80"
+      "https://images.unsplash.com/photo-1490645935967-10de6ba17061?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1453&q=80",
+      [new Ingredient("Vegan", 2), new Ingredient("Buns", 4)]
     ),
   ];
 
