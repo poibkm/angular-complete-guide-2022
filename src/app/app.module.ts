b7f4c4dbd4849925e6d68e2c7b6e43798413f47e
Manuel Brestak <manuel.brestak@porscheinformatik.com>
2022-08-24 13:40:08 +0200

    156. Setting Up Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 import { ShoppingEditComponent } from "./shopping-list/shopping-edit/shopping-edit.component";
 import { FormsModule } from "@angular/forms";
 import { DropdownDirective } from "./shared/dropdown.directive";
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
   declarations: [
@@ -24,7 +25,7 @@ import { DropdownDirective } from "./shared/dropdown.directive";
     ShoppingEditComponent,
     DropdownDirective,
   ],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
