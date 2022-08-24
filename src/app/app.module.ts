5f0913cd8101bda61ea9fed8a395998e1c9789ae
Manuel Brestak <manuel.brestak@porscheinformatik.com>
2022-08-24 09:37:23 +0200

    150. Controlling Navigation with canDeactivate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { PageNotFoundComponent } from "./page-not-found/page-not-found.component
 import { AppRoutingModule } from "./app-routing.module";
 import { AuthService } from "./auth.service";
 import { AuthGuardService } from "./auth-guard.service";
+import { CanDeactivateGuardService } from "./servers/edit-server/can-deactivate-guard.service";
 
 @NgModule({
   declarations: [
@@ -27,7 +28,12 @@ import { AuthGuardService } from "./auth-guard.service";
     PageNotFoundComponent,
   ],
   imports: [BrowserModule, FormsModule, AppRoutingModule],
-  providers: [ServersService, AuthService, AuthGuardService],
+  providers: [
+    ServersService,
+    AuthService,
+    AuthGuardService,
+    CanDeactivateGuardService,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
