2c80480be03ead997bbaf83a69aa0ce4c1f143a2
Manuel Brestak <manuel.brestak@porscheinformatik.com>
2022-08-24 10:36:19 +0200

    152. Resolving Dynamic Data with the resolve Guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,8 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AuthService } from "./auth.service";
 import { AuthGuardService } from "./auth-guard.service";
 import { CanDeactivateGuardService } from "./servers/edit-server/can-deactivate-guard.service";
-import { ErrorPageComponent } from './error-page/error-page.component';
+import { ErrorPageComponent } from "./error-page/error-page.component";
+import { ServerResolver } from "./servers/server/server-resolver.service";
 
 @NgModule({
   declarations: [
@@ -35,6 +36,7 @@ import { ErrorPageComponent } from './error-page/error-page.component';
     AuthService,
     AuthGuardService,
     CanDeactivateGuardService,
+    ServerResolver,
   ],
   bootstrap: [AppComponent],
 })
--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute, Params, Router } from "@angular/router";
+import { ActivatedRoute, Data, Params, Router } from "@angular/router";
 
 import { ServersService } from "../servers.service";
 
@@ -18,12 +18,16 @@ export class ServerComponent implements OnInit {
   ) {}
 
   ngOnInit() {
+    this.route.data.subscribe((data: Data) => {
+      this.server = data["server"];
+    });
+
     // + sign to convert to a number
-    const id = +this.route.snapshot.params["id"];
+    /* const id = +this.route.snapshot.params["id"];
     this.server = this.serversService.getServer(id);
     this.route.params.subscribe((params: Params) => {
       this.server = this.serversService.getServer(+params["id"]);
-    });
+    }); */
   }
 
   onEdit() {
