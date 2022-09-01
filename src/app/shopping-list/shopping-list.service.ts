a76f0e41f7065775dcfeb6779baa09bd88247a13
Manuel Brestak <manuel.brestak@porscheinformatik.com>
2022-09-01 10:46:14 +0200

    221. Allowing the Selection of Items in the List

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,4 +1,4 @@
-import {  Injectable } from "@angular/core";
+import { Injectable } from "@angular/core";
 import { Subject } from "rxjs";
 import { Ingredient } from "../shared/ingredient.model";
 
@@ -7,6 +7,7 @@ import { Ingredient } from "../shared/ingredient.model";
 })
 export class ShoppingListService {
   ingredientsChanged = new Subject<Ingredient[]>();
+  startedEditing = new Subject<number>();
   private ingredients: Ingredient[] = [
     new Ingredient("Onions", 1),
     new Ingredient("Tomatoes", 10),
