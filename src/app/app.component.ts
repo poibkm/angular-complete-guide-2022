40e83f3af0d9482a9f254c97b0e4660e4cd27534
Manuel Brestak <manuel.brestak@porscheinformatik.com>
2022-09-02 08:26:46 +0200

    242. Using Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,42 @@
-import { Component, OnDestroy, OnInit } from "@angular/core";
-import { Subscription } from "rxjs";
-import { UserService } from "./user.service";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
-  userActivated = false;
-  private activatedSub: Subscription;
-
-  constructor(private userService: UserService) {}
-
-  ngOnInit() {
-    this.activatedSub = this.userService.activatedEmitter.subscribe(
-      (didActivate) => {
-        this.userActivated = didActivate;
-      }
-    );
-  }
-
-  ngOnDestroy(): void {
-    this.activatedSub.unsubscribe();
+export class AppComponent {
+  servers = [
+    {
+      instanceType: 'medium',
+      name: 'Production Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'large',
+      name: 'User Database',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'small',
+      name: 'Development Server',
+      status: 'offline',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'small',
+      name: 'Testing Environment Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    }
+  ];
+  getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}) {
+    return {
+      'list-group-item-success': server.status === 'stable',
+      'list-group-item-warning': server.status === 'offline',
+      'list-group-item-danger': server.status === 'critical'
+    };
   }
 }
