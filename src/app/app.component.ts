675ac57e75d8625c356da4852a090d36724b3cf6
Manuel Brestak <manuel.brestak@porscheinformatik.com>
2022-09-02 10:49:10 +0200

    256. Sending a POST Request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,67 +1,28 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  appStatus = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve("stable");
-    }, 2000);
-  });
-  servers = [
-    {
-      instanceType: "medium",
-      name: "Production Server",
-      status: "stable",
-      started: new Date(15, 1, 2017),
-    },
-    {
-      instanceType: "large",
-      name: "User Database",
-      status: "stable",
-      started: new Date(15, 1, 2017),
-    },
-    {
-      instanceType: "small",
-      name: "Development Server",
-      status: "offline",
-      started: new Date(15, 1, 2017),
-    },
-    {
-      instanceType: "small",
-      name: "Testing Environment Server",
-      status: "stable",
-      started: new Date(15, 1, 2017),
-    },
-  ];
+export class AppComponent implements OnInit {
+  loadedPosts = [];
 
-  filteredStatus = "";
+  constructor(private http: HttpClient) {}
 
-  getStatusClasses(server: {
-    instanceType: string;
-    name: string;
-    status: string;
-    started: Date;
-  }) {
-    return {
-      "list-group-item-success": server.status === "stable",
-      "list-group-item-warning": server.status === "offline",
-      "list-group-item-danger": server.status === "critical",
-    };
+  ngOnInit() {}
+
+  onCreatePost(postData: { title: string; content: string }) {
+    // Send Http request
+    console.log(postData);
   }
 
-  onAddServer() {
-    this.servers.push({
-      instanceType: "small",
-      name: "New Server",
-      status: "offline",
-      started: new Date(),
-    });
+  onFetchPosts() {
+    // Send Http request
+  }
 
-    // Suggestion to avoid setting pure pipe to false
-    this.servers = this.servers.slice();
+  onClearPosts() {
+    // Send Http request
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppComponent } from "./app.component";
-import { ShortenPipe } from "./shorten.pipe";
-import { FilterPipe } from './filter.pipe';
+import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [AppComponent, ShortenPipe, FilterPipe],
-  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
