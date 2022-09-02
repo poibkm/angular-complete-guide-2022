f0a50218a92684586a5fcf9b8575d26547a61a85
Manuel Brestak <manuel.brestak@porscheinformatik.com>
2022-09-02 10:49:14 +0200

    256. Sending a POST Request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
   loadedPosts = [];
+  private DATABASE_URL =
+    "https://ng-complete-guide-f22ba-default-rtdb.europe-west1.firebasedatabase.app";
 
   constructor(private http: HttpClient) {}
 
@@ -16,6 +18,12 @@ export class AppComponent implements OnInit {
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
     console.log(postData);
+    // Angular will transform it automatically to JSON
+    this.http
+      .post(`${this.DATABASE_URL}/posts.json`, postData)
+      .subscribe((responseData) => {
+        console.log(responseData);
+      });
   }
 
   onFetchPosts() {
