ce455e9979efcbdcaac31306dafc1a1b01f7924a
Manuel Brestak <manuel.brestak@porscheinformatik.com>
2022-09-05 13:53:00 +0200

    285. Resolving Data Before Loading

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { map } from "rxjs";
+import { map, tap } from "rxjs";
 import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../recipes/recipe.service";
 
@@ -24,28 +24,26 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http
-      .get<Recipe[]>(`${this.DB_URL}/recipes.json`)
-      .pipe(
-        // map => rxjs operator
-        map((recipes) => {
-          // map is called on an array; normal JS map method
-          return recipes.map((recipe) => {
-            /* return {
+    return this.http.get<Recipe[]>(`${this.DB_URL}/recipes.json`).pipe(
+      // map => rxjs operator
+      map((recipes) => {
+        // map is called on an array; normal JS map method
+        return recipes.map((recipe) => {
+          /* return {
               ...recipe,
               ingredients: recipe.ingredients ? recipe.ingredients : [],
             }; */
 
-            // Merging objects => will be overwritten if it exists
-            return {
-              ingredients: [],
-              ...recipe,
-            };
-          });
-        })
-      )
-      .subscribe((recipes) => {
+          // Merging objects => will be overwritten if it existsr
+          return {
+            ingredients: [],
+            ...recipe,
+          };
+        });
+      }),
+      tap((recipes) => {
         this.recipeService.setRecipes(recipes);
-      });
+      })
+    );
   }
 }
