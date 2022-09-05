660f23b0d393b3d5b15e00ead7a0bbf454f1b807
Manuel Brestak <manuel.brestak@porscheinformatik.com>
2022-09-05 13:10:46 +0200

    282. Storing Recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,9 +1,23 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { RecipeService } from "../recipes/recipe.service";
 
 @Injectable({
   providedIn: "root",
 })
 export class DataStorageService {
-  constructor(private http: HttpClient) {}
+  private DB_URL =
+    "https://ng-complete-guide-f22ba-default-rtdb.europe-west1.firebasedatabase.app/";
+
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    // Depends on API, could also be POST
+    this.http
+      .put(`${this.DB_URL}/recipes.json`, recipes)
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
 }
