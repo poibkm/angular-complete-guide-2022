c3a2541e4f6e5d507d94c3ad4e661dd46299af88
Manuel Brestak <manuel.brestak@porscheinformatik.com>
2022-09-05 10:27:52 +0200

    276. Multiple Interceptors

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -1,11 +1,9 @@
 import {
-  HttpEventType,
   HttpHandler,
   HttpInterceptor,
   HttpRequest,
 } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { tap } from "rxjs/operators";
 
 @Injectable({ providedIn: "root" })
 export default class AuthInterceptorService implements HttpInterceptor {
@@ -13,8 +11,6 @@ export default class AuthInterceptorService implements HttpInterceptor {
     // To Restrict Requests
     if (request.url) {
     }
-    console.log("Request is on its way");
-    console.log(request.url);
     // const modifiedRequest = request.clone({url: 'some-new-url'});
     const modifiedRequest = request.clone({
       headers: request.headers.append("Auth", "xyz"),
@@ -28,14 +24,6 @@ export default class AuthInterceptorService implements HttpInterceptor {
       }
     }); */
 
-    return next.handle(modifiedRequest).pipe(
-      tap((event) => {
-        console.log(event);
-        if (event.type === HttpEventType.Response) {
-          console.log("Response arrived, body data:");
-          console.log(event.body);
-        }
-      })
-    );
+    return next.handle(modifiedRequest);
   }
 }
---
+++ src/app/logging-interceptor.service.ts
@@ -0,0 +1,29 @@
+import {
+  HttpEventType,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { tap } from "rxjs/operators";
+
+@Injectable({
+  providedIn: "root",
+})
+export class LoggingInterceptorService implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    console.log("Outgoing request");
+    console.log(req.url);
+    console.log(req.headers);
+    return next.handle(req).pipe(
+      tap((event) => {
+        if (event.type === HttpEventType.Response) {
+          console.log("Incoming response");
+          console.log(event.body);
+        }
+      })
+    );
+  }
+
+  constructor() {}
+}
