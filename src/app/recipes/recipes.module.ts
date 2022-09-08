41e663b543e075bc54320bb6b1a33cd230d8eb5f
Manuel Brestak <manuel.brestak@porscheinformatik.com>
2022-09-08 14:18:49 +0200

    326. Component Declarations

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -26,13 +26,5 @@ import { RecipesComponent } from "./recipes.component";
     ReactiveFormsModule,
     RecipesRoutingModule,
   ],
-  exports: [
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
-    RecipeStartComponent,
-    RecipeEditComponent,
-  ],
 })
 export class RecipesModule {}
