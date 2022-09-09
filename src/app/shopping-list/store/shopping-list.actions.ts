5af088f7df9a0f6a799d7e633394b6443fa41da2
Manuel Brestak <manuel.brestak@porscheinformatik.com>
2022-09-09 12:05:04 +0200

    362. Dispatching Actions

--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -6,5 +6,5 @@ export const ADD_INGREDIENT = "ADD_INGREDIENT";
 export class AddIngredient implements Action {
   // readonly is a TS feature, that this must never be changed from outside
   readonly type = ADD_INGREDIENT;
-  payload: Ingredient;
+  constructor(public payload: Ingredient) {}
 }
