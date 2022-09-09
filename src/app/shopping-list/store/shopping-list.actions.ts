16d42b09cedb27793f47cdb3daa43ecb2678525f
Manuel Brestak <manuel.brestak@porscheinformatik.com>
2022-09-09 10:15:27 +0200

    359. Understanding & Adding Actions

---
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -0,0 +1,10 @@
+import { Action } from "@ngrx/store";
+import { Ingredient } from "src/app/shared/ingredient.model";
+
+export const ADD_INGREDIENT = "ADD_INGREDIENT";
+
+export class AddIngredient implements Action {
+  // readonly is a TS feature, that this must never be changed from outside
+  readonly type = ADD_INGREDIENT;
+  payload: Ingredient;
+}
