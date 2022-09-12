219cb7795ba4c756367041c39cc47a1b4b13d1f1
Manuel Brestak <manuel.brestak@porscheinformatik.com>
2022-09-12 07:56:59 +0200

    367. Managing More State via NgRx

--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -5,20 +5,25 @@ export const ADD_INGREDIENT = "ADD_INGREDIENT";
 export const ADD_INGREDIENTS = "ADD_INGREDIENTS";
 export const UPDATE_INGREDIENT = "UPDATE_INGREDIENT";
 export const DELETE_INGREDIENT = "DELETE_INGREDIENT";
+export const START_EDIT = "START_EDIT";
+export const STOP_EDIT = "STOP_EDIT";
 
 export class AddIngredient implements Action {
   // readonly is a TS feature, that this must never be changed from outside
   readonly type = ADD_INGREDIENT;
+
   constructor(public payload: Ingredient) {}
 }
 
 export class AddIngredients implements Action {
   readonly type = ADD_INGREDIENTS;
+
   constructor(public payload: Ingredient[]) {}
 }
 
 export class UpdateIngredient implements Action {
   readonly type = UPDATE_INGREDIENT;
+
   constructor(
     public payload: {
       index: number;
@@ -29,11 +34,24 @@ export class UpdateIngredient implements Action {
 
 export class DeleteIngredient implements Action {
   readonly type = DELETE_INGREDIENT;
+
   constructor(public payload: number) {}
+}
+
+export class StartEdit implements Action {
+  readonly type = START_EDIT;
+
+  constructor(public payload: number) {}
+}
+
+export class StopEdit implements Action {
+  readonly type = STOP_EDIT;
 }
 
 export type ShoppingListActions =
   | AddIngredient
   | AddIngredients
   | UpdateIngredient
-  | DeleteIngredient;
+  | DeleteIngredient
+  | StartEdit
+  | StopEdit;
